91f0b148347c91a99e5c97a65e63f9780b89ee04
Calprimus <fulvio.famularo@hotmail.com>
2019-08-07 13:30:05 +0200

    On Master daily 3

--- test-1.js
+++ test-1.js
@@ -7,6 +7,9 @@ else if (age === 25){
     console.log('You are exactly 25 years old!')
 }
 
+else if (age >= 10){
+    console.log('You are more than 10 years old!')
+}
 else if (age <= 25){
     console.log('You are younger than 25 years old!')
 };
